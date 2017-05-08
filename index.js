3205309ea151afbbc2d7bc98704afad0e5574db2
Mohammed Fouad <mohammed@redapesolutions.com>
2017-05-08 10:48:08 +0800

    sdasdasd

---
+++ index.js
@@ -0,0 +1,18 @@
+var twit = require('twit');
+
+var bot = new twit({
+    consumer_key:process.env.TESTBOT_CONSUMER_KEY,
+    consumer_secret:process.env.TESTBOT_CONSUMER_SECRET,
+    access_token:process.env.TESTBOT_ACCESS_TOKEN,
+    access_token_secret:process.env.TESTBOT_ACCESS_TOKEN_SECRET,
+    timeout_ms:60000
+})
+
+bot.post('statuses/update', {status: 'hello twitter API :)'}
+    , function(err, data, response){
+    if (err) {
+        console.log(err);
+    } else{
+        console.log(data.text + 'tweeted ya prince');
+    }
+});
